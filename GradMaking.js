5057ec82749a8f0a38ad9a85bcdeb2c07afe46b1
Rakib Islam <rakibislam.rh330@gmail.com>
2026-08-15 10:57:51 +0600

    Gradmeking

---
+++ GradMaking.js
@@ -0,0 +1,25 @@
+
+
+let grad = 32;
+
+if(grad >= 80 && grad <= 100){
+    console.log("A+");
+}
+else if(grad >= 70 && grad < 80){
+    console.log("A");
+}
+else if(grad >= 60 && grad < 70){
+    console.log("B");
+}
+else if(grad >= 50 && grad < 60){
+    console.log("C");
+}
+else if(grad >= 40 && grad < 50){
+    console.log("D");
+}
+else if(grad >= 33 && grad < 40){
+    console.log("E");
+}
+else{
+    console.log("Fail");
+}
